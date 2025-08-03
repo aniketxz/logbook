5bc1e7e98e2004313de9e98179b4add13fe04269
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-08-03 10:13:04 +0530

    created endpoint for clerk webhook, and integrated user data for sync and security

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import connectDB from './lib/connectDB.js'
 import userRouter from './routes/user.route.js'
 import postRouter from './routes/post.route.js'
 import commentRouter from './routes/comment.route.js'
-import connectDB from './lib/connectDB.js'
+import webhookRouter from './routes/webhook.route.js'
 
 const app = express()
+app.use('/webhooks', webhookRouter)
 app.use(express.json())
 
 // app.get('/test', (req, res) => {
