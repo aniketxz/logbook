6680991d623e127cca0916c088ab8d4fa0f5bb00
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-09-23 21:31:51 +0530

    layout fixes

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 const Layout = () => {
 	return (
-		<div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+		<div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48'>
 			<Navbar />
 			<Outlet />
 		</div>
